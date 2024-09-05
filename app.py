096552f50ca73cdf60921c9e8d1990256f2f5f9c
elizabethlconn <148993813+elizabethlconn@users.noreply.github.com>
2024-09-05 19:01:29 -0400

    sept 5 commit

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 # Import the dependencies.
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import inspect
+from sqlalchemy import func, Date, cast, Integer
 
+import numpy as np
+import pandas as pd
+import datetime as dt
+from datetime import datetime, timedelta
 
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+conn = engine.connect()
+
+Base = automap_base()
+Base.prepare(autoload_with=engine)
+Base.classes.keys()
 
 
 # reflect an existing database into a new model
@@ -13,7 +30,8 @@
 
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
 
@@ -21,10 +39,20 @@
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def home():
+    print("Server received request for 'Home' page...")
+    return "Welcome to my 'Home' page!"
+
+app.run()
+
+
+
+#### add my endpoints where it says return above "welcome to my home page"
